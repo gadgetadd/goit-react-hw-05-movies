b3db5e8c1da25293f9566b77ff0227de58f68776
Maksym Ostapchuk <gadget.add@gmail.com>
2023-04-30 18:10:10 +0300

    fix styles

--- src/pages/MovieDetails.styled.jsx
+++ src/pages/MovieDetails.styled.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 export const MainWrapper = styled.div`
   display: flex;
   gap: 40px;
-  margin-top: 20px;
   border-bottom: 2px solid whitesmoke;
   padding: 20px;
 
